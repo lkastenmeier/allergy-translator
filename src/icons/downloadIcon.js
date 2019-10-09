89d1a54702e2e5e78c0b3520b0118a0bd6e37b2d
Lena Kastenmeier <l.kastenmeier@web.de>
2019-10-09 19:20:36 +0200

    add icons

---
+++ src/icons/downloadIcon.js
@@ -0,0 +1,21 @@
+import React from "react";
+export default function DownloadIcon({ className }) {
+  return (
+    <svg
+      id="Icon_ionic-md-download"
+      data-name="Icon ionic-md-download"
+      xmlns="http://www.w3.org/2000/svg"
+      width="18.5"
+      height="23"
+      viewBox="0 0 18.5 23"
+    >
+      <path
+        id="Icon_ionic-md-download-2"
+        data-name="Icon ionic-md-download"
+        d="M25.25,12.616H19.966V4.5H12.034v8.116H6.75L16,22.085ZM6.75,24.793V27.5h18.5V24.793Z"
+        transform="translate(-6.75 -4.5)"
+        fill="#e3ebee"
+      />
+    </svg>
+  );
+}
