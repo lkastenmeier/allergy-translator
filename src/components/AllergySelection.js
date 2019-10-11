f6e1a736bf2845516a98400e8a556b39f5f1a737
Lena Kastenmeier <l.kastenmeier@web.de>
2019-10-11 15:13:29 +0200

    add Eventlistener to allergyselection that changes style onClick

--- src/components/AllergySelection.js
+++ src/components/AllergySelection.js
@@ -1,19 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import AllergyName from "../components/AllergyName";
 import Picture from "./Picture";
 
-const AllergySelectionBox = styled.nav`
+const AllergySelectionBox = styled.div`
   display: flex;
   flex-direction: column;
-  border: 3px solid #e3ebee;
   width: 146px;
   height: 137px;
 `;
 
 export default function AllergySelection({ src, alt, name }) {
+  const [activeBox, setBox] = useState(false);
   return (
-    <AllergySelectionBox>
+    <AllergySelectionBox
+      onClick={() => setBox(true)}
+      style={{
+        border: activeBox ? " 3px solid #2D5F73" : "3px solid #e3ebee"
+      }}
+      name={name}
+    >
       <Picture src={src} alt={alt} />
       <AllergyName name={name}>{name}</AllergyName>
     </AllergySelectionBox>
