31328126d5a7bfff7224759af8fb5ee76d5753c4
Lena Kastenmeier <l.kastenmeier@web.de>
2019-10-13 15:11:12 +0200

    add router for all pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import styled from "styled-components";
 import GlobalStyles from "./GlobalStyles";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Card from "./pages/Card";
+import Help from "./pages/Help";
+import Change from "./pages/Change";
+import Info from "./pages/Info";
 
 const Container = styled.div`
   width: 100vw;
@@ -17,7 +21,10 @@ function App() {
       <GlobalStyles />
       <Router>
         <Route path="/" exact component={Home} />
-        {/* <Route path="/home" exact component={Home} /> */}
+        <Route path="/card" exact component={Card} />
+        <Route path="/help" exact component={Help} />
+        <Route path="/change" exact component={Change} />
+        <Route path="/info" exact component={Info} />
       </Router>
     </Container>
   );
