d727bb05571657c4539eb0a0367d5831221d830d
Lena Kastenmeier <l.kastenmeier@web.de>
2019-10-15 12:11:44 +0200

    add warningpictures and display them on home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,27 +4,30 @@ import Navigation from "../components/Navigation";
 import AllergySelectionContainer from "../components/AllergySelectionContainer";
 import AllergySelection from "../components/AllergySelection";
 import H1 from "../components/H1";
-const allergies = [
-  "milk",
-  "eggs",
-  "treenuts",
-  "peanuts",
-  "wheat",
-  "shellfish",
-  "fish",
-  "soy"
-];
+const allergies = {
+  milk: "../../public/warning-images/milk.svg",
+  eggs: "../../public/warningImages/eggs.svg",
+  treenuts: "/src/warning-images/treenuts.svg",
+  peanuts: "src/warning-images/peanuts.svg",
+  wheat: "public/warning-images/wheat.svg",
+  shellfish: "src/warning-images/shellfish.svg",
+  fish: "src/warning-images/fish.svg",
+  soy: "src/warning-images/soy.svg"
+};
+
 export default function Home() {
   return (
-    <body>
+    <>
       <Header />
       <Navigation selected="0" />
       <H1 />
       <AllergySelectionContainer>
-        {allergies.map(allergy => (
-          <AllergySelection name={allergy} key={allergy} />
+        {Object.keys(allergies).map(key => (
+          <AllergySelection key={key} name={key} src={allergies[key]}>
+            {key}
+          </AllergySelection>
         ))}
       </AllergySelectionContainer>
-    </body>
+    </>
   );
 }
