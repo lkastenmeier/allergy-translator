f9f238ca96a41f632c0e92967ba4e8bf32eb3586
Lena Kastenmeier <l.kastenmeier@web.de>
2019-10-30 11:20:14 +0100

    change h1 into title component

--- src/pages/Info.js
+++ src/pages/Info.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Header from "../components/Header";
 import Navigation from "../components/Navigation";
-import H1 from "../components/H1";
+import Title from "../components/Title";
 
 export default function Home() {
   return (
     <body>
       <Header />
       <Navigation selected="2" />
-      <H1 name="Here will be some Info such as FAQ, Instructions and Impressum" />
+      <Title name="Here will be some Info such as FAQ, Instructions and Impressum" />
     </body>
   );
 }
