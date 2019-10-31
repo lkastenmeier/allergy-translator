c96bf5d14c992ba3a56207538a27a4078b20885b
Lena Kastenmeier <l.kastenmeier@web.de>
2019-10-31 17:09:50 +0100

    add useEffect to start-animation

--- src/pages/Start.js
+++ src/pages/Start.js
@@ -2,10 +2,13 @@ import React, { useState } from "react";
 import StartScreen from "../components/StartScreen";
 import { useHistory } from "react-router-dom";
 
-export default function Home() {
+export default function StartAnimation() {
   const history = useHistory();
   const [toMain, setToMain] = useState(false);
-  setTimeout(() => setToMain(true), 3200);
-
+  React.useEffect(() => {
+    const timeout = () => setTimeout(() => setToMain(true), 3100);
+    timeout();
+    return clearTimeout(timeout);
+  }, []);
   return <>{toMain ? history.push("/main") : <StartScreen />}</>;
 }
