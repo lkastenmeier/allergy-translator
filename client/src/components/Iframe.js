86d38e15c86effa0a8c01203333e9d015a08fb5b
lkastenmeier <54039198+lkastenmeier@users.noreply.github.com>
2019-11-03 15:51:34 +0100

    Map (#21)
    * embed map with preset hospital-search and searchbar
    
    * remove dead code
    
    * add minor styling-adjustments
    
    * save iframe styling in variable
    
    * change function name
    
    * change component  name

---
+++ client/src/components/Iframe.js
@@ -0,0 +1,35 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+const apiKey = `${process.env.REACT_APP_GOOGLE_KEY}`;
+
+const Frame = styled.article`
+  margin: auto;
+  width: 97%;
+  height: 97%;
+  border-radius: 2px;
+  border: 3px solid ${props => props.theme.light};
+`;
+const iframeStyle = {
+  border: `0`,
+  margin: `auto`,
+  width: `100%`,
+  height: `100%`
+};
+
+export default function Iframe({ location }) {
+  return (
+    <Frame>
+      <iframe
+        title="hospitals"
+        location={location}
+        style={iframeStyle}
+        src={`https://www.google.com/maps/embed/v1/search?&q=hospital+near+${location}&key=${apiKey}`}
+      />
+    </Frame>
+  );
+}
+
+Iframe.propTypes = {
+  location: PropTypes.string
+};
---
+++ client/src/components/Searchfield.js
@@ -0,0 +1,27 @@
+import React from "react";
+import styled from "styled-components";
+import PropTypes from "prop-types";
+
+const InputField = styled.input`
+  display: flex;
+  text-align-last: center;
+  width: 50%;
+  min-height: 50px;
+  margin: 10px auto 5px auto;
+  background-color: ${props => props.theme.light};
+  border: 2px solid ${props => props.theme.highlight};
+  color: ${props => props.theme.text};
+  font-size: 16px;
+`;
+
+export default function SearchField({ input }) {
+  function onLocationInput(event) {
+    const { value } = event.target;
+    input(value);
+  }
+  return <InputField placeholder="Your Location" onChange={onLocationInput} />;
+}
+
+Searchfield.propTypes = {
+  onChange: PropTypes.func
+};
